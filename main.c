805c26ed96da77fcae1bdb9e2602aa6082c852ce
LnrdRmj <leonardo.ra14@icloud.com>
2022-05-07 12:37:34 +0200

    Ora il main crea i 16 file per i segmenti nella cartella segmenti

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include "stdio.h"
+#include "fcntl.h"
+#include <string.h>
+
+#define NUMERO_TRATTE 16
+#define NUMERO_PROCESSI_TRENI 5
+
+// Questo file rappresenta il processo padre
+
+void creaFiles(void);
+void inizializzaProcessiTreni(void);
 
 int main() {
 
-  printf("ciao\nciao anche a te\n");
+  // open("cartella/text.txt", O_CREAT|O_RDWR, 0777);
+
+  // FILE *file = fopen("segmenti/segmenti.txt", "w");
+  // fclose(file);
+
+  creaFiles();
+
+}
+
+void creaFiles() {
+
+  printf("Sto creando i file\n");
+  for (int i = 0; i < NUMERO_TRATTE; i++) {
+
+    char *pathName;
+    asprintf(&pathName, "segmenti/MA%d", i);
+    // printf("%s\n", buf);
+    // int file = open(buf, O_CREAT|O_RDWR, 0666);
+
+    FILE *file = fopen(pathName, "w+");
+
+    fwrite("0", 1, 1, file);
+
+    fclose(file);
+    chmod(pathName, 0666);
+
+  }
 
 }
